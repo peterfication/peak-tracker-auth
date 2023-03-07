16e4e012b0d4b8f92f00fc8722b0498b0456940c
Peter Gundel <mail@petergundel.de>
2023-03-07 15:42:38 +0100

    Add Doorkeeper and Doorkeeper OpenID Connect

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   root "pages#root"
 
   devise_for(:users, path: :auth, controllers: { sessions: "users/sessions" })
+  use_doorkeeper do
+    # TODO: Skip not needed controllers
+    #
+    # it accepts :authorizations, :tokens, :token_info, :applications and :authorized_applications
+    # skip_controllers :tokens, :applications, :token_info
+  end
+  use_doorkeeper_openid_connect
 
   get :dashboard, to: "pages#dashboard"
 end
