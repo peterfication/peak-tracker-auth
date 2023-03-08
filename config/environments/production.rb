69c0034752d9896fa895bb3203ebe16d06aab3a3
Peter Gundel <mail@petergundel.de>
2023-03-08 17:03:55 +0100

    Add SparkPost config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: "auth.peak-tracker.com" }
+  config.action_mailer.delivery_method = :sparkpost
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
