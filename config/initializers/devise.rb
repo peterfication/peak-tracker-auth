5e257672993a8bf321108907f87175350736f66d
Peter Gundel <mail@petergundel.de>
2023-03-08 17:07:32 +0100

    Fix credentials usage in initializers
    In tests, they can be nil

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  config.secret_key = Rails.application.credentials.devise.secret_key
+  config.secret_key = Rails.application.credentials.devise&.secret_key
 
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
@@ -14,7 +14,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = Rails.application.credentials.devise.sender.email
+  config.mailer_sender = Rails.application.credentials.devise&.sender&.email
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
@@ -116,7 +116,7 @@ Devise.setup do |config|
   config.stretches = Rails.env.test? ? 1 : 12
 
   # Set up a pepper to generate the hashed password.
-  config.pepper = Rails.application.credentials.devise.pepper
+  config.pepper = Rails.application.credentials.devise&.pepper
 
   # Send a notification to the original email when the user's email is changed.
   config.send_email_changed_notification = true
--- config/initializers/spark_post.rb
+++ config/initializers/spark_post.rb
@@ -1,5 +1,5 @@
 SparkPostRails.configure do |c|
-  c.api_key = Rails.application.credentials.spark_post.api_key
+  c.api_key = Rails.application.credentials.spark_post&.api_key
   c.api_endpoint = "https://api.eu.sparkpost.com/api/"
   c.transactional = true
 end
