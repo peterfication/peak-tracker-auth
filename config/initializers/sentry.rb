14963aeb3d512e9ca051308292b550945dba1b8f
Peter Gundel <mail@petergundel.de>
2023-04-01 08:06:01 +0200

    Only report Sentry errors in production

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,14 +1,16 @@
-Sentry.init do |config|
-  config.dsn = Rails.application.credentials.sentry&.dsn
+if Rails.env.production?
+  Sentry.init do |config|
+    config.dsn = Rails.application.credentials.sentry&.dsn
 
-  config.breadcrumbs_logger = %i[active_support_logger http_logger]
+    config.breadcrumbs_logger = %i[active_support_logger http_logger]
 
-  # Set traces_sample_rate to 1.0 to capture 100%
-  # of transactions for performance monitoring.
-  # We recommend adjusting this value in production.
-  config.traces_sample_rate = 1.0
-  # OR
-  # config.traces_sampler = lambda do |_context|
-  #   true
-  # end
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    config.traces_sample_rate = 1.0
+    # OR
+    # config.traces_sampler = lambda do |_context|
+    #   true
+    # end
+  end
 end
