077b354363200b48aea43ebb95c6ca0e5a20e351
Peter Morgenstern <mail@petergundel.de>
2023-09-23 12:44:40 +0200

    Clean up wisper initializer

--- app/jobs/new_user_notification_job_v2.rb
+++ app/jobs/new_user_notification_job_v2.rb
@@ -3,6 +3,10 @@
 class NewUserNotificationJobV2 < ApplicationJob
   queue_as :default
 
+  def self.models_user_created(user)
+    NewUserNotificationJobV2.perform_later({ "id" => user.id })
+  end
+
   def perform(*args)
     Rails.logger.info "New user available: #{args.inspect}"
   end
--- config/initializers/wisper.rb
+++ config/initializers/wisper.rb
@@ -1,10 +1,3 @@
 Rails.application.reloader.to_prepare do
-  Wisper.subscribe(
-    Class.new do
-      def models_user_created(user)
-        NewUserNotificationJobV2.perform_later({ "id" => user.id })
-      end
-    end.new,
-    scope: User, on: :models_user_created,
-  )
+  Wisper.subscribe(NewUserNotificationJobV2, scope: User, on: :models_user_created)
 end
